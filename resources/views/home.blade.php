54ab7f9995b750aae5446e6742050795100a2ef2
Marianna Galuppi <marianna.galuppi@gmail.com>
2022-04-08 19:46:32 +0700

    aggiunta view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.standard')
 
 @section('content')
-    HOME
+    <h1>{{$tour['name']}}</h1>
+    <p>DESTINATION: {{$tour['destination']}}</p>
+    <p>DURATION: {{$tour['duration']}}</p>
+    <p>PRICE: {{$tour['price']}}</p>
+    <p>THEME: {{$tour['theme']}}</p>
+    <p>CLASS: {{$tour['class']}}</p>
+    <p>OVERVIEW: {{$tour['overview']}}</p>
+    <small>REFERENCE CODE: {{$tour['reference_code']}}</small>
 @endsection
